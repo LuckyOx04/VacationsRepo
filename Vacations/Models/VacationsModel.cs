a913e8094932c84b3b3521e03889182c4198c1d7
ivailon27@gmail.com <ivailon27@gmail.com>
2021-11-09 17:21:02 +0200

    Add project files.

---
+++ Vacations/Models/VacationsModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vacations.Models
+{
+    public class VacationsModel
+    {
+        public int NumberOfPeople { get; }
+        public string Destination { get; }
+        public string TransportType { get; }
+        public int HotelStars { get; }
+        public string Rooms { get; }
+        public bool IsRoomFree { get; }
+        public decimal PriceOfBedForAdult { get; }
+        public decimal PriceOfBedForChildren { get; }
+        public int Number { get; }
+    }
+}
---
+++ Vacations/dbo.Vacantions.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Vacantions] (
+    [Number Of People]          INT             NULL,
+    [Destination]               NVARCHAR (50)   NULL,
+    [Transport Type]            NVARCHAR(50)             NULL,
+    [Hotel Stars]               INT             NULL,
+    [Rooms]                     NVARCHAR(50)      NULL,
+    [Is Room Free]              BIT             NULL,
+    [Price Of Bed For Adult]    DECIMAL (18, 2) NULL,
+    [Price Of Bed For Children] DECIMAL (18, 2) NULL,
+    [Number]                    INT             NOT NULL,
+    PRIMARY KEY CLUSTERED ([Number]),
+    UNIQUE NONCLUSTERED ([Number] ASC)
+);
+
